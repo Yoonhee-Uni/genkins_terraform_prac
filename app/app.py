e4438b50c84f4ee29d357a8672203b9ee87812a4
Yoonhee-Uni <yoonheeleeuk@gmail.com>
2024-10-28 22:15:15 +0000

    push

--- app/app.py
+++ app/app.py
@@ -1,12 +1,10 @@
-# from flask import Flask
+from flask import Flask
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route("/")
-# def hello():
-#     return "Hello, CI/CD Pipeline!"
+@app.route("/")
+def hello():
+    return "<h1>Hello, World!</h1>"
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000)
-
-print("hello world")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
